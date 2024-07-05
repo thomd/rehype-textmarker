2228f3e7ff0044ea3a0dbc927b50dc61e6a634b8
Thomas Dürr <thomduerr@gmail.com>
2024-07-05 14:17:32 +0200

    prettier

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ import { visit } from 'unist-util-visit'
 import { findAndReplace } from 'hast-util-find-and-replace'
 
 const rehypeTextmarker = (options) => {
-  if (options !== null) {
-    if (!Array.isArray(options)) {
-      options = [options]
-    }
-    return (tree) => {
-      for (let option of options) {
-        visit(
-          tree,
-          (node) => {
-            return option.tags ? option.tags.includes(node.tagName) : node.tagName == 'p'
-          },
-          (node) => {
-            findAndReplace(node, [
-              option.textPattern,
-              (value, capture, match) => {
-                const markNode = {
-                  type: 'element',
-                  tagName: option.htmlTag != null ? option.htmlTag : 'mark',
-                  properties: option.className != null ? { className: option.className } : {},
-                  children: [{ type: 'text', value: capture }],
-                }
-                return markNode
-              },
-            ])
-          }
-        )
+   if (options !== null) {
+      if (!Array.isArray(options)) {
+         options = [options]
       }
-    }
-  }
+      return (tree) => {
+         for (let option of options) {
+            visit(
+               tree,
+               (node) => {
+                  return option.tags ? option.tags.includes(node.tagName) : node.tagName == 'p'
+               },
+               (node) => {
+                  findAndReplace(node, [
+                     option.textPattern,
+                     (value, capture, match) => {
+                        const markNode = {
+                           type: 'element',
+                           tagName: option.htmlTag != null ? option.htmlTag : 'mark',
+                           properties: option.className != null ? { className: option.className } : {},
+                           children: [{ type: 'text', value: capture }],
+                        }
+                        return markNode
+                     },
+                  ])
+               }
+            )
+         }
+      }
+   }
 }
 export default rehypeTextmarker
